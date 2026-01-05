e55d1700daf41d08fe5fff31cc463945f014eb6a
khalilmedjoudj <medjoudjkhalilcsc9@gmail.com>
2026-01-05 16:09:18 +0100

    Refactor event listeners for login and publish actions

--- js/script.js
+++ js/script.js
@@ -1,8 +1,14 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const wrapper = document.querySelector(".wrapper");
-    const loginBtn = document.querySelector(".login-btn");
-    const publishLink = document.querySelector(".btn-publish");
-    const closeBtn = document.querySelector(".icon-close");
-    const registerLink = document.querySelector(".register-link");
-    const loginLink = document.querySelector(".login-link");
-    const user = JSON.parse(localStorage.getItem("user"));
+ const openPopup = (e) => {
+        if (e) e.preventDefault();
+        if (wrapper) wrapper.classList.add("show");
+    };
+    if (loginBtn && !user) {
+        loginBtn.addEventListener("click", openPopup);
+    }
+    if (publishLink) {
+        publishLink.addEventListener("click", (e) => {
+            e.preventDefault();
+            if (user) {
+                window.location.href = "ajouter.html";
+            } else {
+                openPopup();
